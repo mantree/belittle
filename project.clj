5096ac48e15f51dcef5d29b3d85ba19a64fbc15d
Tom Coupland <tcoupland@gmail.com>
2016-03-01 17:09:52 +0000

    Tidy up project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject belittle "0.5.0-SNAPSHOT"
   :description "Bringing Mocking to core.test since 2016"
-  :url "https://github.com/mixradio/belittle"
-  :license {:name "New BSD License"
-            :url "https://github.com/mixradio/belittle/blob/master/LICENSE"}
+  :url "https://github.com/mantree/belittle"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [medley "0.7.0"]]
   :profiles {:dev {:dependencies [[com.gfredericks/test.chuck "0.2.5"]]}})
